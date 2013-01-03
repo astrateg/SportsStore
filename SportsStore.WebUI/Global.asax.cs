25c34e2f71ff5b54abe90236e0b5bc76ef4ca279
alex <alex@iboox.ru>
2013-01-03 23:14:30 +0200

    Вернул примечание

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -10,6 +10,9 @@ using System.Web.UI;
 
 namespace SportsStore.WebUI
 {
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
